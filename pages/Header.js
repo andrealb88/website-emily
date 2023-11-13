ce2bafc31e1b4c00c860b61e44be91fd5ed642a6
Your Name <you@example.com>
2023-11-13 12:47:23 +0000

    header.css actualization

--- pages/Header.js
+++ pages/Header.js
@@ -3,13 +3,23 @@
 // }
 
 // Header.js
-
+import "/styles/header.css";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "/pages/important-pages/logo2.jpg";
 function Header() {
   return (
     <header>
+      <Image
+        alt="women2"
+        className="logo-header"
+        src={logo}
+        width="100%"
+        height="90%"
+      />
       <nav>
         <Link href="/">Home</Link>
+
         <Link href="important-pages/About">About</Link>
 
         <Link href="important-pages/blog">Blog</Link>
